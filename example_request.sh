cb954863e67cce22e9f3804e30b27ecff542930e
Michael Dresser <michaelmdresser@gmail.com>
2019-10-18 13:21:48 -0600

    better example request that just reads current ssh key

--- example_request.sh
+++ example_request.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-curl -XPOST localhost:8080/container -H "Content-Type: application/json" --data "@testbody.json"
+#curl -XPOST localhost:8080/container -H "Content-Type: application/json" --data "@testbody.json"
+curl -XPOST localhost:8080/container -H "Content-Type: application/json" --data "{ \"ssh_public_key\": \"$(cat ~/.ssh/id_rsa.pub)\" }"
